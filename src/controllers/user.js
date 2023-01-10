429d65c283da4c627efd5b832fe8d8bc3b99d532
fescarvalho <fernando_scarvalho@hotmail.com>
2023-01-10 18:19:17 -0300

    :sparkles: feat - Create migrations of user and config email to  unique.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,36 +1,20 @@
 import { User } from "../models";
-import * as Yup from "yup";
-import bcrypt from "bcrypt";
 
-class UserControllers {
+class UserController {
   async create(req, res) {
-    try {
-      const schema = Yup.object().shape({
-        name: Yup.string()
-          .required("Name is mandatory.")
-          .min(3, "Name must be at least 3 characters."),
-        email: Yup.string().email("Email is invalid.").required("Email is mandatory."),
-        password: Yup.string()
-          .required("Password is mandatory.")
-          .min(6, "Password must be at least 6 characters."),
-      });
+    const user = new User({
+      name: "Fernando",
+      email: "fescarv@teste.com",
+      password: "teste123",
+      password_hash: "",
+      reset_password_token: "teste",
+      reset_password_token_sent_at: new Date(),
+      avatar_url: "testeURl",
+    });
+    await user.save();
 
-      await schema.validate(req.body);
-
-      const hashPassword = await bcrypt.hash(req.body.password, 8);
-      const user = new User({
-        ...req.body,
-        password: "",
-        password_hash: hashPassword,
-      });
-
-      await user.save();
-
-      return res.json({ user });
-    } catch (error) {
-      return res.status(400).json({ error: error?.message });
-    }
+    return res.json({ user });
   }
 }
 
-export default new UserControllers();
+export default new UserController();
