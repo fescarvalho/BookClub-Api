678f7ab1df48e482465a34ed2ff1c95420fc876f
fescarvalho <fernando_scarvalho@hotmail.com>
2023-01-10 18:53:44 -0300

    :sparkles: feat - login implemented.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
 import db from "./src/models";
 import UserControllers from "./src/controllers/user";
 
@@ -6,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/", UserControllers.create);
+app.post("/login", UserControllers.login);
 app.listen(3333, async () => {
   try {
     await db.sequelize.authenticate();
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,35 @@
 import { User } from "../models";
 import * as Yup from "yup";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 class UserControllers {
+  async login(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().email("Email is invalid.").required("Email is mandatory."),
+        password: Yup.string().required("Password is mandatory."),
+      });
+      await schema.validate(req.body);
+
+      const user = await User.findOne({ where: { email: req.body.email } });
+      if (!user) return res.status(401).json({ error: "User not found." });
+
+      const checkPassword = await bcrypt.compare(req.body.password, user.password_hash);
+      if (!checkPassword) return res.status(401).json({ error: "Email or password do not match." });
+
+      console.log({ hash: process.env.JWT_HASH });
+      const token = jwt.sign({ id: user.id }, process.env.JWT_HASH, {
+        expiresIn: 2592000, //30 dias
+      });
+      const { id, name, email, avatar_url, createdAt } = user;
+      return res.json({
+        user: [id, name, email, avatar_url, createdAt, token],
+      });
+    } catch (error) {
+      return res.status(400).json({ error: error?.message });
+    }
+  }
   async create(req, res) {
     try {
       const schema = Yup.object().shape({
@@ -15,10 +42,9 @@ class UserControllers {
           .min(6, "Password must be at least 6 characters."),
       });
 
+      await schema.validate(req.body);
       const existedUser = await User.findOne({ where: { email: req.body.email } });
       if (existedUser) return res.status(404).json({ error: "User already exists." });
-
-      await schema.validate(req.body);
 
       const hashPassword = await bcrypt.hash(req.body.password, 8);
       const user = new User({
