021cec0e716eb0c4f4299930c178a2f827efeb63
fescarvalho <fernando_scarvalho@hotmail.com>
2023-01-10 18:26:28 -0300

    :sparkles: feat - finish user register.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,39 @@
 import { User } from "../models";
+import * as Yup from "yup";
+import bcrypt from "bcrypt";
 
-class UserController {
+class UserControllers {
   async create(req, res) {
-    const user = new User({
-      name: "Fernando",
-      email: "fescarv@teste.com",
-      password: "teste123",
-      password_hash: "",
-      reset_password_token: "teste",
-      reset_password_token_sent_at: new Date(),
-      avatar_url: "testeURl",
-    });
-    await user.save();
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .required("Name is mandatory.")
+          .min(3, "Name must be at least 3 characters."),
+        email: Yup.string().email("Email is invalid.").required("Email is mandatory."),
+        password: Yup.string()
+          .required("Password is mandatory.")
+          .min(6, "Password must be at least 6 characters."),
+      });
 
-    return res.json({ user });
+      const existedUser = await User.findOne({ where: { email: req.body.email } });
+      if (existedUser) return res.status(404).json({ error: "User already exists." });
+
+      await schema.validate(req.body);
+
+      const hashPassword = await bcrypt.hash(req.body.password, 8);
+      const user = new User({
+        ...req.body,
+        password: "",
+        password_hash: hashPassword,
+      });
+
+      await user.save();
+
+      return res.json({ user });
+    } catch (error) {
+      return res.status(400).json({ error: error?.message });
+    }
   }
 }
 
-export default new UserController();
+export default new UserControllers();
